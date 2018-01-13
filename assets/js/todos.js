e32abafff040ca07fe7581d5edce72928fc18db5
sasaber <s.ahmsab@gmail.com>
2018-01-12 20:52:17 -0800

    styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -20,6 +20,10 @@ $("input[type='text']").keypress(function(event){
 		$(this).val("");
 		// create new li with the text equal to that value
 		// and add it to the list
-		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");
 	}
+});
+
+$(".fa-pencil").click(function(){
+	$("input[type='text']").fadeToggle();
 });
